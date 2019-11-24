88fb11984b5ea909dfa6c964fb8aa8b63bd744a3
Emma Bowen <emloubow@gmail.com>
2019-11-24 19:47:20 +0000

    adding mapbox

--- app/controllers/towns_controller.rb
+++ app/controllers/towns_controller.rb
@@ -4,12 +4,16 @@ require 'open-uri'
 class TownsController < ApplicationController
   # before_action :set_town, only: [show, scraping]
   def index
-    @towns = Town.all
+    @towns = Town.geocoded
     @town = @towns.sample
   end
 
   def show
     @town = Town.find(params[:id])
+    @markers = {
+      lat: @town.latitude,
+      lng: @town.longitude
+    }
     url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=#{@town.name}&limit=1&format=json"
     town_serialized = open(url).read
     @town_detail = JSON.parse(town_serialized)
